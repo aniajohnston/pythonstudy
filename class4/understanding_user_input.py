52b683a38b0217d561731d765735e2a5a2bd9a0e
aniajohnston <ajohns81@syr.edu>
2024-03-08 12:28:53 -0500

    new files for user input

---
+++ class4/understanding_user_input.py
@@ -0,0 +1,19 @@
+'''User Input'''
+
+'''User input is always of the 'string' data type'''
+#this doesn't store the input but it will appear in the terminal.
+# input("Enter Your name: ")
+
+#we have to assign our input to variables:
+#we can assign variables and print them one after the other:
+# name = input("Enter your name: ")
+# # age = input("Enter your age: ")
+# # age += 1 #doesn't work because it is a string
+# '''you have to type cast the input to an integer'''
+# # age = int(age)
+
+# '''FOR EFFICIENCY'''
+# age = int(input('Type in your age: '))
+# # age += 1
+# print(f'Hello {name}')
+# print(f'You are {age} years old.')
