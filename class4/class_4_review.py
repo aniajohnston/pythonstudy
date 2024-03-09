340f1fa2e73838d0fed9f5ea7c8288b2e880efb3
aniajohnston <ajohns81@syr.edu>
2024-03-08 20:56:21 -0500

    added a practice section for tomorrow

---
+++ class4/class_4_review.py
@@ -0,0 +1,56 @@
+'''
+Take a word from the user. Then take a number from the user. Then print whether the word is longer than the number.
+
+Examples:
+apple
+6
+False
+
+python
+4
+True
+'''
+
+
+
+'''
+Exercise 
+
+Write some code that takes a string from the user, and prints how many vowels are in the string. (Vowels are a, e, i, o, u)
+How will you count both uppercase and lowercase vowels?
+What string method can you use to count the number of vowels?
+
+Example
+User input: Computer
+3
+
+'''
+
+
+
+# replace() Returns a string where a specified value is replaced with a specified value
+
+
+ex_18 = 'Today is Tuesday. Tuesday we play golf.' # replace instances of Tuesday with Thursday
+
+
+# split() Splits the string at the specified separator, and returns a list
+test_18 = 'I will be broken up on every space'
+
+
+ex_19 = 'Split*me*up*on*the*asterisk' # split this spring up on every asterisk
+
+# splitlines() Splits the string at line breaks and returns a list
+lyrics = "Every time that I look in the mirror\nAll these lines on my face getting clearer\nThe past is gone\nOh, it went by like dusk to dawn\nIsn't that the way?"
+
+
+
+
+# join() Joins the elements of an iterable to the end of the string
+
+#use -
+my_colors = ['blue', 'green', 'red', 'orange', 'blue']
+
+
+# use *
+ex_16 = ['summer', 'spring', 'fall', 'winter'] # create a string from this list and separate it with an asterisk
