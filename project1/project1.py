dadcad1ece1e9865ff0c5602754904af46a98dc7
aniajohnston <ajohns81@syr.edu>
2024-03-11 22:06:09 -0400

    working capitalize check

--- project1/project1.py
+++ project1/project1.py
@@ -13,12 +13,14 @@ while True:
     username = input('f\nPlease type your username.\n It must start with a lowercase\n only contain letters, numbers, and underscores\n Taken usernames are: {taken}\n\n USERNAME:')
     password = input(f'\nPlease type your password.\n It must contain at least 8 characters\n at least one uppercase letter\n at least one lowercase letter\n at least one digit\n one of these characters:{characters}\n cannot contain spaces \n\n PASSWORD:')
     #the username must not be in the list of taken usernames
-    if username.capitalize():
-          print('Your username must not have a capital')
-          break
-    elif username in taken:
-            print(username, error_messages[1])
-            break
+    if username != username.capitalize():
+          print('Your username is good')
+    else:
+         print('BAD')
+         break
+    # elif username in taken:
+    #         print(username, error_messages[1])
+    #         break
 '''username requirements:
 no spaces
 must start with a lowercase letter
