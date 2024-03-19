fbfb3962b0864476b22ac0f7bd9a5f5aed52ff6b
aniajohnston <ajohns81@syr.edu>
2024-03-19 17:27:39 -0400

    changed file name

--- project1/project1.py
+++ project1/project1.py
@@ -1,71 +1,54 @@
+import re
+
+'''Project Description: Website Sign-Up'''
+
+'''Write a program that prompts the user to sign up for a website. 
+The user must come up with a username and password, then log in.
+'''
+
+'''USERNAME REQUIREMENTS'''
+'''The username must:
+Follow all the Python conventions for a variable name.
+1. It must start with a lowercase letter
+2.It must only contain letters, numbers, and underscores.
+3.It must not be in the list of taken usernames:
+Taken usernames = "admin", "admin123", "superuser", "superuser123"
+If the username does not meet these requirements, print:
+"Invalid Username" or "Username Taken" and continue looping
+
+Test data
+
+'''
 
 
+username, password = '',''
 
 error_messages = ['Invalid Username:','Username Taken:', 'Invalid Password:', 'Sign up sucessful!', 'Incorrect Username or Password:', 'Login sucessful!']
-username_characters = ['!','?','@','#','$','^','&','*','-']
-password_characters = ['!','?','@','#','$','^','&','*','_','-']
-
-
-username = ''
-password = ''
 taken = ['admin', 'admin123', 'superuser', 'superuser123']
 
 
+
 while True:
-    username = input('\nPlease type your username.\n It must start with a lowercase\n only contain letters, numbers, and underscores\n Taken usernames are: {taken}\n\n USERNAME:')
-    #must be uniform
-    username = username.strip()
-    #must start with lowercase
-    if username[0].islower():
-        print(f'Test Passed: {username} starts with a lowercase.')
+    username = input('\nPlease enter your username.\nIt must follow python conventions(only contain letters, numbers, and underscores)\nIt must not be in the list of taken usernames:\n\n\t"admin", "admin123", "superuser", "superuser123"\n\nUsername: ')
+    #must start with a lowercase letter
+    first_letter = username[0]
+    lower_case_test = first_letter.islower()
+    if lower_case_test:
+        print(f'Test Passed: {username} starts with a lowercase letter.')
+    
     else:
-        print(f'{error_messages[0]}')
+        print(f'{error_messages[0]} {username} does not start with a lowercase letter.')
         continue
-
-
-
-
-    # the username must not be in the list of taken usernames
+    #contains letters, numbers, underscores
+    py_conventions = bool(re.match("^[A-Za-z0-9_-]*$", username))
+    if py_conventions:
+        print(f'Test Passed: {username} follows python conventions.')
+    else:
+        print(f'{error_messages[0]} {username} does not follow python conventions.')
+        continue
+    #must not be in the list of taken usernames
     if username not in taken:
         print(f'Test Passed: {username} is not in the list of taken usernames.')
     else:
-        print(username, error_messages[1])
-        break
-
-
-    # must contain numbers and letters
-    for char in username:
-        if not char.isalnum():
-            print(username, error_messages[0])
-            continue
-    # password = input(f'\nPlease type your password.\n It must contain at least 8 characters\n at least one uppercase letter\n at least one lowercase letter\n at least one digit\n one of these characters:{characters}\n cannot contain spaces \n\n PASSWORD:')
-
-   
-
-    #must only include an underscore
-    # for char in username
-   
-    # else:
-    #     continue
-'''username requirements:
-only use underscores
-'''
-
-
-'''password requirements:
-at least 8 characters long
-at least one uppercase letter
-at least one lowercase letter
-at least one digit
-contain at least one special characters
-doesn't contain spaces
-'''
-
-'''password in while true'''
-'''try login prompt'''
-
-
-#password contains at least one special character
-# for p in password:
-#     if p in characters:
-#         continue
+        print(f'{error_messages[1]} {username} is in the list of taken usernames.')
+        continue
